b247dbb69f3079a22007084510790735c90dd19f
Kian Gabriel Arambulo <bg117@sooosilly.xyz>
2023-12-09 10:48:10 +0800

    add supabase client

---
+++ utils/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://akzhjllnroxfqeufcrjc.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFremhqbGxucm94ZnFldWZjcmpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIwMjc1NTUsImV4cCI6MjAxNzYwMzU1NX0.t2orqSvo_7FcKBcX9zj5V6oo7A7cWyPZWrtcl72mXJ0';
+
+export const client = createClient(supabaseUrl, supabaseKey)
