0d60747e69dbf199905e6ba5cde27466a153669f
Kian Gabriel Arambulo <bg117@sooosilly.xyz>
2023-12-09 10:53:51 +0800

    check auth status in navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,18 @@
+'use client'
+
 import Link from "next/link";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {client} from "@/utils/supabase";
 
 export function Navbar() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        client.auth.onAuthStateChange((_, session) => {
+            setLoggedIn(session !== null);
+        })
+    }, []);
+
     return <nav className="navbar navbar-expand-lg bg-success-subtle">
         <div className="container">
             <Link className="navbar-brand" href="/">dpBoom!</Link>
@@ -12,12 +23,16 @@ export function Navbar() {
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <Link className="nav-link active" href="/login">Login</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link active" href="/register">Register</Link>
-                    </li>
+                    {loggedIn ? <li className="nav-item">
+                        <Link className="nav-link active" href="/events">Events</Link>
+                    </li> : <>
+                        <li className="nav-item">
+                            <Link className="nav-link active" href="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" href="/register">Register</Link>
+                        </li>
+                    </>}
                 </ul>
             </div>
         </div>
