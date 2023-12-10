634ce5e805115ed76aab6419eed74dc3c1ff6112
Kian Gabriel Arambulo <bg117@sooosilly.xyz>
2023-12-10 13:36:05 +0800

    center call to action

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './globals.css'
+import './globals.scss'
 
 export default function RootLayout({
                                        children,
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import React, {useCallback} from "react";
 import {Content} from "@/components/content";
-import {Button} from "react-bootstrap";
+import {Button, Col, Row} from "react-bootstrap";
 import {useRouter} from "next/navigation";
 
 export default function Home() {
@@ -12,10 +12,14 @@ export default function Home() {
 
     return (
         <Content>
-            <h1>dpBoom!</h1>
-            <p>dpBoom! is a simple, easy to use, and free to use profile frame generator, designed to be
-                a replacement for Twibbonize.</p>
-            <Button color="primary" onClick={navigateToEvents}>Get Started</Button>
+            <Row className="flex-grow-1 d-flex flex-column justify-content-center align-content-center">
+                <Col lg={8}>
+                    <h1>dpBoom!</h1>
+                    <p>dpBoom! is a simple, easy to use, and free to use profile frame generator, designed to be
+                        a replacement for Twibbonize.</p>
+                    <Button color="primary" onClick={navigateToEvents}>Get Started</Button>
+                </Col>
+            </Row>
         </Content>
     );
 }
