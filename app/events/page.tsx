1e6e1c1b8b921ab76c38b8b66ab792d36370715b
Kian Gabriel Arambulo <bg117@sooosilly.xyz>
2023-12-17 18:33:41 +0800

    add event creator

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -4,6 +4,7 @@ import {Content} from "@/components/content";
 import {useSession} from "@/hooks/session";
 import {Button, Card, Col, Row} from "react-bootstrap";
 import {useGetEntries} from "@/hooks/entries";
+import Link from "next/link";
 
 export default function Events() {
     const {session} = useSession();
@@ -13,20 +14,19 @@ export default function Events() {
         <h1>Events</h1>
         {session &&
             <div className="d-inline-block mb-4">
-                <Button href="/events/create">Create Event</Button>
+                <Button href="/events/create" variant="outline-primary">Create Event</Button>
             </div>}
-        {isLoading && <h2>Loading...</h2>}
-        {isError && <h2>{error?.message}</h2>}
+        {isLoading && <h5>Loading...</h5>}
+        {isError && <h5>{error?.message}</h5>}
         <Row>
             {data?.map((event) =>
                 <Col lg={4} md={6} key={event.slug} className="mb-4 d-flex flex-column">
                     <Card className="flex-grow-1">
                         <Card.Body>
                             <Card.Title>{event.name}</Card.Title>
-                            {event.ends_at &&
-                                <Card.Subtitle className="mb-2 text-muted">Ends at {new Date(event.ends_at).toLocaleString()}</Card.Subtitle>}
+                            <Card.Subtitle className="mb-2 text-muted">by {event.display_name}</Card.Subtitle>
                             <Card.Text>{event.caption}</Card.Text>
-                            <Button href={`/events/${event.slug}`}>View Event</Button>
+                            <Link className="stretched-link" href={`/events/${event.slug}`}>View Event</Link>
                         </Card.Body>
                     </Card>
                 </Col>)}
