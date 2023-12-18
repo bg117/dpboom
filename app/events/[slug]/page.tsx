e7a99e5cdc35c3095aae056d727ed6fe9e3b6cfb
Kian Gabriel Arambulo <bg117@sooosilly.xyz>
2023-12-18 17:40:02 +0800

    fix download add social media links

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -3,52 +3,81 @@
 import {Content} from "@/components/content";
 import {useGetEvent} from "@/hooks/events";
 import {Button, Col, FormControl, Image as Im, Row} from "react-bootstrap";
-import {useCallback, useEffect, useMemo, useState} from "react";
-import fileDownload from "js-file-download";
+import {useCallback, useEffect, useRef, useState} from "react";
 
 export default function Slug({params: {slug}}: { params: { slug: string } }) {
     const {data, isLoading, isError, error} = useGetEvent(slug);
     const [imgSrc, setImgSrc] = useState<string>(data?.frame ?? '');
     const [img, setImg] = useState<HTMLImageElement | null>(null);
-    const canvas = useMemo(() => document.createElement('canvas'), []);
-    const ctx = useMemo(() => canvas.getContext('2d'), [canvas]);
-    const frame = useMemo(() => {
-        const img = new Image();
-        img.src = data?.frame!;
-        return img;
-    }, [data]);
+    const [frame, setFrame] = useState<HTMLImageElement | null>(null);
+
+    const canvasRef = useRef(document.createElement("canvas"));
+    const inputRef = useRef(document.createElement("input"));
+    const aRef = useRef(document.createElement("a"));
+    const ctx = useRef(canvasRef.current.getContext('2d')!);
+
     const copyClick = useCallback(async () => {
-        await navigator.clipboard.writeText(data!.caption!);
+        if (!data?.caption) {
+            return;
+        }
+
+        await navigator.clipboard.writeText(data.caption);
     }, [data]);
+
+    const inputOnChange = useCallback((e: any) => {
+        const file = (e.target as HTMLInputElement).files![0];
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const img = new Image();
+            img.src = e.target!.result as string;
+            setImg(img);
+        };
+        reader.readAsDataURL(file);
+    }, []);
+
     const uploadClick = useCallback((e: any) => {
         // show file dialog
         e.preventDefault();
-        const input = document.createElement('input');
-        input.type = 'file';
-        input.accept = 'image/*';
-        input.onchange = (e) => {
-            const file = (e.target as HTMLInputElement).files![0];
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                const img = new Image();
-                img.src = e.target!.result as string;
-                setImg(img);
-            };
-            reader.readAsDataURL(file);
-        };
-        input.click();
+        inputRef.current?.click();
     }, []);
+
     const downloadClick = useCallback(() => {
-        fileDownload(imgSrc, `${data!.name}.png`);
-    }, [data, imgSrc]);
+        aRef.current?.click();
+    }, []);
+
+    inputRef.current.type = 'file';
+    inputRef.current.accept = 'image/*';
+    inputRef.current.hidden = true;
+    inputRef.current.onchange = inputOnChange;
+
+    aRef.current.hidden = true;
+    aRef.current.download = 'frame.png';
+    aRef.current.href = imgSrc;
 
     useEffect(() => {
-        canvas.width = frame.width;
-        canvas.height = frame.height;
-
-        if (!img) {
+        if (!data?.frame) {
             return;
         }
+
+        const f = new Image();
+        f.src = data.frame;
+
+        canvasRef.current.width = f.width;
+        canvasRef.current.height = f.height;
+
+        ctx.current.drawImage(f, 0, 0);
+
+        setImgSrc(canvasRef.current.toDataURL());
+        setFrame(f);
+    }, [data]);
+
+    useEffect(() => {
+        if (!frame || !img) {
+            return;
+        }
+
+        // clear canvas
+        ctx.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 
         // draw img in the center of the canvas
         const x = (frame.width - img.width) / 2;
@@ -56,16 +85,12 @@ export default function Slug({params: {slug}}: { params: { slug: string } }) {
         // zoom img to fit frame
         const scale = Math.max(frame.width / img.width, frame.height / img.height);
         // draw img on canvas
-        ctx!.drawImage(img, 0, 0, img.width, img.height, x, y, img.width * scale, img.height * scale);
+        ctx.current.drawImage(img, 0, 0, img.width, img.height, x, y, img.width * scale, img.height * scale);
         // draw frame on top of img
-        ctx!.drawImage(frame, 0, 0);
+        ctx.current.drawImage(frame, 0, 0);
 
-        setImgSrc(canvas.toDataURL());
-    }, [canvas, ctx, frame, img]);
-
-    useEffect(() => {
-        setImgSrc(data?.frame ?? '');
-    }, [data]);
+        setImgSrc(canvasRef.current.toDataURL());
+    }, [img, frame]);
 
     if (isLoading) {
         return <Content>
@@ -84,16 +109,12 @@ export default function Slug({params: {slug}}: { params: { slug: string } }) {
         <h1>{data!.name}</h1>
 
         {/* readonly textarea filled with caption with copy button */}
-        <Row className="g-2">
-            <Col md={8}>
-                <div className="flex-grow-1">
-                    <FormControl as="textarea" readOnly value={data!.caption!} className="mb-2" rows={10}/>
-                    <Button variant="outline-secondary" onClick={copyClick}>Copy</Button>
-                </div>
-            </Col>
-
+        <Row className="g-4">
             <Col md={4}>
-                <Im fluid src={imgSrc} className="mb-2" alt="frame"/>
+                <h6 className="text-muted">Frame</h6>
+
+                <Im fluid src={imgSrc} alt="frame" className="mb-2"/>
+
                 <div className="d-flex gap-2 flex-column flex-sm-row">
                     <Button variant="secondary" className="w-100" onClick={uploadClick}>Upload</Button>
                     <Button variant="outline-secondary" className="w-100" disabled={!img} onClick={downloadClick}>
@@ -101,6 +122,22 @@ export default function Slug({params: {slug}}: { params: { slug: string } }) {
                     </Button>
                 </div>
             </Col>
+            <Col md={8} className="d-flex flex-column">
+                <h6 className="text-muted">Caption</h6>
+                <FormControl as="textarea" readOnly value={data!.caption!} className="mb-2" rows={10}/>
+                <Button variant="outline-secondary" onClick={copyClick}>Copy</Button>
+            </Col>
         </Row>
+        <hr/>
+        <h5>Share this Event</h5>
+        <p>Share this event with your friends and family!</p>
+        <div className="d-flex gap-2">
+            <Button variant="outline-primary" href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`}>
+                Facebook
+            </Button>
+            <Button variant="outline-primary" href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(data!.caption!)}&url=${encodeURIComponent(window.location.href)}`}>
+                Twitter
+            </Button>
+        </div>
     </Content>;
 }
